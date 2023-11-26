c95b0310a8965914065da77264489e987c08ddd8
azhe <richazhe@qq.com>
2023-11-26 22:57:59 +0800

    add many

--- src/main/java/com/azhe/tagster/controller/fileUploadController.java
+++ src/main/java/com/azhe/tagster/controller/fileUploadController.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartRequest;
 
 import javax.annotation.Resource;
 import java.io.File;
@@ -40,39 +42,42 @@ public class fileUploadController {
     //文件存储地址
     @Value("${file.upload.dir}") // 从配置文件中读取文件上传目录
     private String uploadDir;
+
     @PostMapping("/file")
-    public Result<?> fileUpload(@RequestParam("file") MultipartFile file,@RequestParam("projectId") Integer projectId) throws IOException {
+    public Result<?> fileUpload(@RequestParam("file") MultipartFile file, @RequestParam("projectId") Integer projectId) throws IOException {
         //获取文件原始名称
         String originalFilename = file.getOriginalFilename();
-        System.out.println("文件名是："+ originalFilename);
+        System.out.println("文件名是：" + originalFilename);
         //获取文件的类型
         String type = FileUtil.extName(originalFilename);
         System.out.println("文件类型是：" + type);
         //定义一个文件唯一标识码（UUID）
         String uuid = UUID.randomUUID().toString();
         //修改文件名，防止文件名冲突
-        String newFileName = uuid+"."+type;
+        String newFileName = uuid + "." + type;
         //保存文件
-        try{
+        try {
             FileUploadUtil.saveFile(newFileName, file);
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             return Result.fail("上传失败");
         }
         //文件信息存入数据库
-        String fileAddress = uploadDir+newFileName;
-        FilesUploadService.insertUploadFile(projectId,originalFilename,type,fileAddress,uuid);
+        String fileAddress = uploadDir + newFileName;
+        FilesUploadService.insertUploadFile(projectId, originalFilename, type, fileAddress, uuid);
         //存入文本内容表
-        if (type.equals("doc") || type.equals("docx")){
-            String content =  extractContentUtil.readDoc(fileAddress);
-            textProjectService.insertTextContent(projectId, uuid,content);
+        if (type.equals("doc") || type.equals("docx")) {
+            String content = extractContentUtil.readDoc(fileAddress);
+            textProjectService.insertTextContent(projectId, uuid, content);
         }
         return Result.ok("上传成功");
 
     }
+
     @PostMapping("/files")
-    public Result<?> filesUpload(@RequestParam("files") MultipartFile[] files,Integer projectId) throws IOException {
+    public Result<?> filesUpload( MultipartFile[] files, Integer projectId) throws IOException {
         //获取文件原始名称
+//        System.out.println("上传文件");
+        System.out.println("projectId:"+projectId);
         for (MultipartFile file : files) {
             String originalFilename = file.getOriginalFilename();
             //获取文件的类型
@@ -82,20 +87,20 @@ public class fileUploadController {
             //定义一个文件唯一标识码（UUID）
             String uuid = UUID.randomUUID().toString();
             //修改文件名，防止文件名冲突
-            String newFileName = uuid+"."+type;
+            String newFileName = uuid + "." + type;
             //保存文件
             try {
                 FileUploadUtil.saveFile(newFileName, file);
                 //存入文件信息到数据库
-                String fileAddress = uploadDir+newFileName;
-                FilesUploadService.insertUploadFile(projectId,originalFilename,type,fileAddress,uuid);
-                if (type.equals("doc") || type.equals("docx")){
-                    String content =  extractContentUtil.readDoc(fileAddress);
-                    textProjectService.insertTextContent(projectId, uuid,content);
+                String fileAddress = uploadDir + newFileName;
+                FilesUploadService.insertUploadFile(projectId, originalFilename, type, fileAddress, uuid);
+                if (type.equals("doc") || type.equals("docx")) {
+                    String content = extractContentUtil.readDoc(fileAddress);
+                    textProjectService.insertTextContent(projectId, uuid, content);
                 }
 
             } catch (IOException e) {
-                return Result.fail(originalFilename+"上传失败");
+                return Result.fail(originalFilename + "上传失败");
             }
         }
 
